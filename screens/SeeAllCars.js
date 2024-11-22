cc500d178fc73b846449b3811dcfe0f605453238
tamtran <tamtranngoc224@example.com>
2024-11-22 07:12:27 +0700

    update

---
+++ screens/SeeAllCars.js
@@ -0,0 +1,505 @@
+import React, { useState, useEffect ,useRef} from 'react';
+import {FlatList, Animated,View, Text, Switch, StyleSheet, Image, ScrollView, Modal, TouchableOpacity , ActivityIndicator,Dimensions} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
+
+
+const SeeAllCars = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+  const { width, height } = Dimensions.get("window");
+  const [isOpen, setIsOpen] = useState(false);
+  const animatedHeight = useRef(new Animated.Value(0)).current;
+    const navigation = useNavigation();
+
+
+
+
+
+
+  const handleOptionPress = () => {
+    setModalVisible(true);
+  };
+
+  const headerBackgroundColor = scrollY.interpolate({
+    inputRange: [0, 150],
+    outputRange: ['rgba(255, 255, 255, 1)', 'rgba(255, 255, 255, 0)'],
+    extrapolate: 'clamp'
+  });
+
+  const headerScale = scrollY.interpolate({
+    inputRange: [0, 150],
+    outputRange: [1, 0.9],
+    extrapolate: 'clamp'
+  });
+  const headerHeight = scrollY.interpolate({
+    inputRange: [0, 150],
+    outputRange: [80, 60],
+    extrapolate: 'clamp'
+  });
+
+  const rotate = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '40deg'],
+    extrapolate: 'clamp'
+  });
+
+
+
+const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+    Animated.timing(rotateAnim, {
+      toValue: isMenuOpen ? 0 : 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  };
+const toggleDropdown = () => {
+    if (isOpen) {
+      Animated.timing(animatedHeight, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start(() => setIsOpen(false));
+    } else {
+      setIsOpen(true);
+      Animated.timing(animatedHeight, {
+        toValue: 250, // Set to your desired dropdown height
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }
+  };
+
+   const hanldeLogoPress = ()=>{
+   toggleDropdown();
+   toggleMenu();
+ }
+
+
+
+
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://66ff37092b9aac9c997e8a42.mockapi.io/BMW');
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => navigation.navigate(item.id)}>
+      <View style={styles.itemContainer}>
+        <Image source={{ uri: item.imgLink }} style={styles.image} />
+        <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.price}>{item.price}</Text>
+        <Text style={styles.gasType}>{item.gasType}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+
+
+
+
+
+  return (
+    <View style={[styles.container, styles.lightBackground]}>
+
+      <Animated.View 
+              style={[
+            styles.header,
+          {
+            backgroundColor: headerBackgroundColor,
+            height: headerHeight,
+            transform: [{ scale: headerScale }]
+          }
+        ]}
+      
+      >
+        <TouchableOpacity onPress={hanldeLogoPress}>
+            <Animated.Image
+             source={require('../logo1.png')}
+            style={[styles.logo, { transform: [{ rotate }] }]}
+          />
+        </TouchableOpacity>
+
+
+        <View style={styles.verticalSeparator} />
+
+        <View style={styles.optionsContainer}>
+          <TouchableOpacity onPress={() => alert('Call')}>
+            <Ionicons
+              style={styles.lightText2}
+              name="call" size={24} color="black"
+            />
+            <Text style={styles.lightText2}>
+              Call
+            </Text>
+          </TouchableOpacity>
+
+          <View style={styles.verticalSeparator} />
+
+          <TouchableOpacity onPress={handleOptionPress}>
+            <Ionicons
+              style={styles.lightText}
+              name="options" size={24} color="black"
+            />
+            <Text style={styles.lightText2}>
+              Menu
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+
+      <View style={styles.separator} />
+
+      <Animated.ScrollView contentContainerStyle={styles.content}
+              onScroll={Animated.event(
+                [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                { useNativeDriver: false }
+              )}
+              scrollEventThrottle={15}
+      >
+
+
+
+// dropdown
+
+            <View style={styles.dropdownCtn}>
+              {isOpen && (
+                <Animated.View style={[styles.dropdown, { height: animatedHeight }]}>
+                  <TouchableOpacity onPress={() => navigation.navigate('DangKy')} style={styles.dropdownItem}><Text>Book A Test Drive</Text></TouchableOpacity >
+
+                  <TouchableOpacity onPress={() => navigation.navigate('DangKy')}  style={styles.dropdownItem}><Text>View all cars</Text></TouchableOpacity >
+        
+                    <TouchableOpacity onPress={() => navigation.navigate('DangKy')}  style={styles.dropdownItem}><Text>Drive Test</Text></TouchableOpacity >
+          
+                    <TouchableOpacity onPress={() => navigation.navigate('DangKy')}  style={styles.dropdownItem}><Text>Drive Test</Text></TouchableOpacity >
+          
+                    <TouchableOpacity onPress={() => navigation.navigate('DangKy')}  style={styles.dropdownItem}><Text>Drive Test</Text></TouchableOpacity>
+          
+                
+                </Animated.View>
+              )}
+            </View>
+
+
+      {/* main Item */}
+
+
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.list}
+        />
+      )}
+    </View>
+
+           {/* footer */}
+        
+        <View style={styles.footer}>
+  <View style={styles.topSection}>
+    <TouchableOpacity style={styles.iconContainer}>
+      <FontAwesome name="car" size={24} color="black" />
+      <Text style={styles.iconText}>Book a Test Drive</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.iconContainer}>
+      <FontAwesome name="list-alt" size={24} color="black" />
+      <Text style={styles.iconText}>BMW Price List</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.iconContainer}>
+      <FontAwesome name="phone" size={24} color="black" />
+      <Text style={styles.iconText}>Contact Dealer</Text>
+    </TouchableOpacity>
+  </View>
+
+  <View style={styles.bottomSection}>
+    <View style={styles.contactColumn}>
+      <Text style={styles.columnTitle}>CONTACT</Text>
+      <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/contact')}>
+        <Text style={styles.linkText}>Contact BMW</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/dealer')}>
+        <Text style={styles.linkText}>Contact Dealer</Text>
+      </TouchableOpacity>
+      <View style={styles.socialIcons}>
+        <View style={styles.socialIconsCTN}>
+          <FontAwesome name="facebook" size={24} color="black" />
+        </View>
+        <View style={styles.socialIconsCTN}>
+          <FontAwesome name="instagram" size={24} color="black" />
+        </View> 
+        <View style={styles.socialIconsCTN}>
+          <FontAwesome name="youtube" size={24} color="black" />
+        </View>
+      </View>
+    </View>
+
+    <View style={styles.moreColumn}>
+      <Text style={styles.columnTitle}>MORE ABOUT BMW</Text>
+      <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/learn-more')}>
+        <Text style={styles.linkText}>Learn more BMW</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => Linking.openURL('https://www.press.bmwgroup.com/asia')}>
+        <Text style={styles.linkText}>BMW PressClub Asia</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+
+  <View style={styles.bottomLinks}>
+    <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/legal')}>
+      <Text style={styles.linkText}>Legal Disclaimer</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/privacy')}>
+      <Text style={styles.linkText}>Privacy Policy</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/cookies')}>
+      <Text style={styles.linkText}>Cookie Policy</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => Linking.openURL('https://www.bmw.com/imprint')}>
+      <Text style={styles.linkText}>Imprint</Text>
+    </TouchableOpacity>
+  </View>
+
+  <Text style={styles.copyright}>© THACO AUTO 2024</Text>
+</View>
+
+
+      </Animated.ScrollView>
+
+      <Modal visible={modalVisible} transparent={true}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text style={styles.closeModal}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  list: {
+    paddingBottom: 20,
+  },
+  itemContainer: {
+    marginBottom: 20,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '100%',
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  price: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  gasType: {
+    fontSize: 14,
+    color: '#666',
+  },
+
+  dropdownCtn:{
+    position:'absolute',
+    top:0,
+    width:"100%",
+    height:250,
+  alignItems:'center',
+  zIndex:1,
+
+  },
+    dropdown: {
+    width:'80%',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    alignItems:'cecnter',
+    justifyContent:'center',
+    elevation: 5, // Android shadow
+    shadowOffset: { width: 0, height: 2 }, // Shadow direction
+    shadowOpacity: 0.25, // Shadow transparency
+    shadowRadius: 3.84, // Shadow blur radius
+  },
+  dropdownItem: {
+    paddingVertical:10,
+    width:"100%",
+    justifyContent:'center',
+    alignItems:'center',
+
+  },
+  
+  container: {
+    paddingTop:20,
+    flex: 1,
+    width:"100%",
+
+  },
+  header: {
+    paddingTop:20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+  },
+  optionsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  verticalSeparator: {
+    width: 1,
+    height: 40,
+    backgroundColor: '#cccccc',
+    marginHorizontal: 10,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'gray',
+    marginHorizontal: 15,
+  },
+  content: {
+    alignItems: 'center',
+    padding: 0,
+  },
+  lightBackground: {
+    backgroundColor: '#ffffff',
+  },
+
+  lightText: {
+    color: '#000000',
+    marginVertical: 2,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  lightText2: {
+    color: '#000000',
+    marginVertical: 2,
+    textAlign: 'center',
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  footer: {
+  width:'100%',
+  backgroundColor: '#f8f8f8',
+  padding: 20,
+},
+topSection: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  marginBottom: 20,
+},
+iconContainer: {
+  alignItems: 'center',
+
+},
+iconText: {
+  marginTop: 5,
+  fontSize: 14,
+},
+bottomSection: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  marginBottom: 20,
+},
+contactColumn: {
+  flex: 1,
+},
+columnTitle: {
+  fontSize: 16,
+  fontWeight: 'bold',
+  marginBottom: 10,
+},
+linkText: {
+  fontSize: 14,
+  color: '#007aff',
+  marginBottom: 5,
+},
+socialIcons: {
+  flexDirection: 'row',
+  marginTop: 10,
+  justifyContent:'center',
+  alignItems:'center',
+},
+moreColumn: {
+  flex: 1,
+},
+
+
+bottomLinks: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  flexWrap: 'wrap',
+  marginBottom: 10,
+},
+copyright: {
+  fontSize: 12,
+  color: '#555',
+
+  marginTop: 10,
+},
+socialIconsCTN:{
+  padding:20,
+},
+logo: {
+  width: 50,
+  height: 50,
+},
+});
+
+
+
+export default SeeAllCars;
